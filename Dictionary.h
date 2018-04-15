ac58524c89512ec9c47b94c13ab0681c58e73bcf
Kara Busse <kbusse6057@gmail.com>
2018-04-14 20:19:46 -0500

    Initial commit

---
+++ Dictionary.h
@@ -0,0 +1,64 @@
+//
+// Created by Kara on 4/10/18.
+//
+
+#ifndef SPELLCHECKER_DICTIONARY_H
+#define SPELLCHECKER_DICTIONARY_H
+
+#endif //SPELLCHECKER_DICTIONARY_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct AVLTreeNode
+{
+    string data;
+    int height;
+    AVLTreeNode* left;
+    AVLTreeNode* right;
+};
+
+class Dictionary
+{
+private:
+    AVLTreeNode* root;
+    int size;
+
+public:
+
+    Dictionary();
+    // Creates an empty dictionary;
+
+    Dictionary(const Dictionary& orig);
+    // Copy constructor
+
+    virtual ~Dictionary();
+    // Destructor
+
+    Dictionary& operator=(const Dictionary& orig);
+    // assignment operator
+
+    void AddEntry(string anEntry);
+    // Preconditions: anEntry has a key not already in the dictionary
+    // Postconditions: anEntry has been added to the dictionary
+
+    bool FindEntry(string key);
+    // Postconditions: if key is found in the dictionary, returns true;
+    //   otherwise returns false
+
+    void PrintSorted(ostream& outStream);
+    // Postconditions: has printed contents of the dictionary in order
+
+private:
+
+    void copyDict(const Dictionary& orig);
+    // copies the contents of orig to this dictionary
+
+    void deleteDict();
+    // properly frees all memory occupied by this Dictionary
+
+};
+
+//#endif
---
+++ main.cpp
@@ -0,0 +1,57 @@
+/**
+ * Kara Busse
+ * Spell Checker
+ */
+
+/*
+ * Build Dictionary of correctly spelled words
+ * Read words from file dict.txt in current directory
+ */
+
+/*
+ * Ask user for name of text file to spell check
+ */
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    string filename;
+    cout<<"Enter the name of the text file which you would like to spell check"<<endl;
+    if(filename == "inputBig.txt") {
+        //open big input
+    }
+    else if(filename == "inputSmall.txt") {
+        //open small input
+    }
+    ifstream file;
+    file.open("");
+
+    //Read in file here
+
+    //Perform operations on information with AVL tree
+
+    /*
+     * Check to see if entry is in AVL tree (find entry)
+     * If entry is not in AVL tree, add it (add entry)
+     * else ???
+     */
+
+    //Output data
+}
+
+/*
+ * Print out list of misspelled words and lines where they occur
+ * For each misspelled word, list any words in dictionary that can be obtained by these rules:
+ * 1) Add one letter to word at any position
+ * 2) Remove one letter from word at any position
+ * 3) Switch around characters that are next to each other
+ *
+ * Line numbers start at 1 in files
+ */
+
+/*
+ *
+ */
+
